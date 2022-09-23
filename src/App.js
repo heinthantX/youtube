4d19c90964e3a53b67510c71d0938b0ec077112f
Hein Thant <heinthantx@gmail.com>
2022-09-23 11:24:56 +0630

    build

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Component/Home';
-import { useEffect, useState } from 'react';
-import useOdd from './hook/useOdd';
-import useEven from './hook/useEven';
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [odd, oddIncrease, setOdd] = useOdd({ startPoint: count });
-  const [even, evenIncrease, setEven] = useEven({ startPoint: count });
-
-  useEffect(() => {
-    setCount(odd);
-    setEven(odd);
-  }, [odd]);
-
-  useEffect(() => {
-    setCount(even);
-    setOdd(even);
-  }, [even]);
-
-  return (
-    <div className="flex flex-col h-full justify-center items-center">
-      <h1 className="text-6xl font-bold">oddCount : {count}</h1>
-      <button onClick={oddIncrease}>Odd Increase</button> |
-      <button onClick={evenIncrease}>Even Increase</button>
-    </div>
-  );
+  return <Home />;
 }
 
 export default App;
