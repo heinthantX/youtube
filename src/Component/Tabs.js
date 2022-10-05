1e61d1cd83ccead39b85f6679b664f91716ffd88
Hein Thant <heinthantx@gmail.com>
2022-10-06 00:43:39 +0630

    update

--- src/Component/Tabs.js
+++ src/Component/Tabs.js
@@ -14,32 +14,36 @@
   }
   ```
 */
-const tabs = [
-  { name: 'My Account', href: '#', current: false },
-  { name: 'Company', href: '#', current: false },
-  { name: 'Team Members', href: '#', current: true },
-  { name: 'Billing', href: '#', current: false },
-  { name: 'My Account', href: '#', current: false },
-  { name: 'Company', href: '#', current: false },
-  { name: 'My Account', href: '#', current: false },
-  { name: 'Company', href: '#', current: false },
-  { name: 'My Account', href: '#', current: false },
-  { name: 'Company', href: '#', current: false },
-];
+// const tabs = [
+//   { name: 'My Account', href: '#', current: false },
+//   { name: 'Company', href: '#', current: false },
+//   { name: 'Team Members', href: '#', current: true },
+//   { name: 'Billing', href: '#', current: false },
+//   { name: 'My Account', href: '#', current: false },
+//   { name: 'Company', href: '#', current: false },
+//   { name: 'My Account', href: '#', current: false },
+//   { name: 'Company', href: '#', current: false },
+//   { name: 'My Account', href: '#', current: false },
+//   { name: 'Company', href: '#', current: false },
+// ];
+
+import { useState } from 'react';
 
 function classNames(...classes) {
   return classes.filter(Boolean).join(' ');
 }
 
-export default function Tabs() {
+export default function Tabs({ tabs, tabsClickListener }) {
+  const [currentTab, setCurrentTab] = useState(-1);
+
   return (
     <div>
-      <div className="sm:hidden">
-        <label htmlFor="tabs" className="sr-only">
+      {/* <div className="sm:hidden"> */}
+      {/* <label htmlFor="tabs" className="sr-only">
           Select a tab
-        </label>
-        {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
-        <select
+        </label> */}
+      {/* Use an "onChange" listener to redirect the user to the selected tab URL. */}
+      {/* <select
           id="tabs"
           name="tabs"
           className="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500"
@@ -48,21 +52,26 @@ export default function Tabs() {
           {tabs.map((tab) => (
             <option key={tab.name}>{tab.name}</option>
           ))}
-        </select>
-      </div>
+        </select> */}
+      {/* </div> */}
       <div className="hidden sm:block py-3 w-full fixed z-10 px-8 bg-[#202020] border-y border-gray-600">
         <nav className="flex space-x-4" aria-label="Tabs">
-          {tabs.map((tab) => (
+          {tabs.map((tab, i) => (
             <a
               key={tab.name}
-              href={tab.href}
+              href={'eroer'}
+              onClick={(e) => {
+                e.preventDefault();
+                tabsClickListener(tab.id);
+                setCurrentTab(i);
+              }}
               className={classNames(
-                tab.current
+                currentTab === i
                   ? 'bg-gray-200 text-gray-800'
                   : 'text-gray-200 bg-[#313131] hover:bg-[#626262]',
-                'px-3 py-2 font-medium text-sm rounded-3xl'
+                'px-3 flex items-center font-medium text-sm rounded-3xl'
               )}
-              aria-current={tab.current ? 'page' : undefined}
+              // aria-current={tab.current ? 'page' : undefined}
             >
               {tab.name}
             </a>
--- src/Component/VideoCards.js
+++ src/Component/VideoCards.js
@@ -1,117 +1,121 @@
 /* This example requires Tailwind CSS v2.0+ */
-const posts = [
-  {
-    title: 'Boost your conversion rate',
-    href: '#',
-    category: { name: 'Article', href: '#' },
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
-    date: 'Mar 16, 2020',
-    datetime: '2020-03-16',
-    imageUrl:
-      'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-    readingTime: '1 day ago',
-    author: {
-      name: 'Roel Aufderehar',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    title: 'How to use search engine optimization to drive sales',
-    href: '#',
-    category: { name: 'Video', href: '#' },
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
-    date: 'Mar 10, 2020',
-    datetime: '2020-03-10',
-    imageUrl:
-      'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-    readingTime: '3 weeks ago',
-    author: {
-      name: 'Brenna Goyette',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    title: 'Improve your customer experience',
-    href: '#',
-    category: { name: 'Case Study', href: '#' },
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-    date: 'Feb 12, 2020',
-    datetime: '2020-02-12',
-    imageUrl:
-      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-    readingTime: '1 year ago',
-    author: {
-      name: 'Daniela Metz',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-  {
-    title: 'Improve your customer experience',
-    href: '#',
-    category: { name: 'Case Study', href: '#' },
-    description:
-      'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
-    date: 'Feb 12, 2020',
-    datetime: '2020-02-12',
-    imageUrl:
-      'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
-    readingTime: '2 months ago',
-    author: {
-      name: 'Daniela Metz',
-      href: '#',
-      imageUrl:
-        'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-    },
-  },
-];
+// const posts = [
+//   {
+//     title: 'Boost your conversion rate',
+//     href: '#',
+//     category: { name: 'Article', href: '#' },
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto accusantium praesentium eius, ut atque fuga culpa, similique sequi cum eos quis dolorum.',
+//     date: 'Mar 16, 2020',
+//     datetime: '2020-03-16',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1496128858413-b36217c2ce36?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+//     readingTime: '1 day ago',
+//     author: {
+//       name: 'Roel Aufderehar',
+//       href: '#',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     },
+//   },
+//   {
+//     title: 'How to use search engine optimization to drive sales',
+//     href: '#',
+//     category: { name: 'Video', href: '#' },
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit facilis asperiores porro quaerat doloribus, eveniet dolore. Adipisci tempora aut inventore optio animi., tempore temporibus quo laudantium.',
+//     date: 'Mar 10, 2020',
+//     datetime: '2020-03-10',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+//     readingTime: '3 weeks ago',
+//     author: {
+//       name: 'Brenna Goyette',
+//       href: '#',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     },
+//   },
+//   {
+//     title: 'Improve your customer experience',
+//     href: '#',
+//     category: { name: 'Case Study', href: '#' },
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+//     date: 'Feb 12, 2020',
+//     datetime: '2020-02-12',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+//     readingTime: '1 year ago',
+//     author: {
+//       name: 'Daniela Metz',
+//       href: '#',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     },
+//   },
+//   {
+//     title: 'Improve your customer experience',
+//     href: '#',
+//     category: { name: 'Case Study', href: '#' },
+//     description:
+//       'Lorem ipsum dolor sit amet consectetur adipisicing elit. Sint harum rerum voluptatem quo recusandae magni placeat saepe molestiae, sed excepturi cumque corporis perferendis hic.',
+//     date: 'Feb 12, 2020',
+//     datetime: '2020-02-12',
+//     imageUrl:
+//       'https://images.unsplash.com/photo-1492724441997-5dc865305da7?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1679&q=80',
+//     readingTime: '2 months ago',
+//     author: {
+//       name: 'Daniela Metz',
+//       href: '#',
+//       imageUrl:
+//         'https://images.unsplash.com/photo-1487412720507-e7ab37603c6f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+//     },
+//   },
+// ];
 
-export default function VideoCards() {
+import '../App.css';
+
+export default function VideoCards({ videos }) {
   return (
-    <div className="relative flex gap-10 mt-4 mx-6 ">
-      {posts.map((post) => (
+    <div className="relative grid grid-cols-[repeat(auto-fit,_minmax(300px,_1fr))] mt-4 mx-6 ">
+      {videos.map((video) => (
         <div
-          key={post.title}
+          key={video.title}
           className="flex flex-col flex-1 overflow-hidden rounded-lg shadow-lg"
         >
           <div className="flex-shrink-0">
             <img
-              className="h-48 w-full object-cover"
-              src={post.imageUrl}
+              className="h-48 w-72 object-cover"
+              src={`https://image.tmdb.org/t/p/original${video.poster_path}`}
               alt=""
             />
           </div>
           <div className="flex flex-col  bg-[#121212] py-4">
             <div className="flex">
               <div className="flex-shrink-0">
-                <a href={post.author.href}>
-                  <span className="sr-only">{post.author.name}</span>
+                <a href={'eroieos'}>
+                  <span className="sr-only">{video.media_type}</span>
                   <img
                     className="h-10 w-10 rounded-full"
-                    src={post.author.imageUrl}
+                    src={`https://image.tmdb.org/t/p/original${video.poster_path}`}
                     alt=""
                   />
                 </a>
               </div>
               <div className="ml-3">
                 <p className="text-sm font-medium text-white mb-1">
-                  <a href={post.author.href}>{post.title}</a>
+                  <a href={'eroiero'}>{video.title}</a>
                 </p>
                 <p className="text-xs text-gray-400">
-                  <a>{post.author.name}</a>
+                  <a>{video.media_type || 'Hello'}</a>
                 </p>
                 <div className="flex space-x-1 text-xs text-gray-400">
-                  <time dateTime={post.datetime}>{post.date}</time>
+                  <time dateTime={video.release_date}>
+                    {video.media_type || 'Hello'}
+                  </time>
                   <span aria-hidden="true">&middot;</span>
-                  <span>{post.readingTime}</span>
+                  <span>{video.release_date}</span>
                 </div>
               </div>
             </div>
